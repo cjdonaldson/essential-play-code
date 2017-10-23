7dcd3d2138b31cd205a5da4d0404055b2bd22080
cdonaldson <charles.donaldson@arris.com>
2017-10-22 22:43:03 -0400

    chapter 2 calc work

--- chapter2-calc/app/controllers/CalcController.scala
+++ chapter2-calc/app/controllers/CalcController.scala
@@ -9,14 +9,18 @@ object CalcController extends Controller {
   //  - accept two integers extracted from the URL;
   //  - add them together;
   //  - return a plain text HTTP 200 response containing the result.
-  def add(a: Int, b: Int) = ???
+  def add(a: Int, b: Int) = Action { request =>
+    Ok(s"result of $a + $b is ${a + b}\n")
+  }
 
   // TODO: Create an action called `add`:
   //
   //  - accept two booleans extracted from the URL;
   //  - and them together;
   //  - return a plain text HTTP 200 response containing the result.
-  def and(a: Boolean, b: Boolean) = ???
+  def and(a: Boolean, b: Boolean) = Action { request =>
+    Ok(s"result of $a and $b is ${a && b}\n")
+  }
 
   // TODO: Create an action called `concat`:
   //
@@ -26,14 +30,26 @@ object CalcController extends Controller {
   //  - return a plain text HTTP 200 response containing the result.
   //
   // TIP: Use the `urlDecode` helper method if you need to to decode the .
-  def concat(args: String) = ???
+  def concat(args: String) = Action { request =>
+    val res = urlDecode(args.replace("/",""))
+    Ok(s"result of concat '$args' is '$res'\n")
+  }
 
   // TODO: Create an action called `concat`:
   //
   //  - accept a list of integers extracted from the URL;
   //  - sort the list;
   //  - return a space separated plain text HTTP 200 response of the result.
-  def sort(a: List[Int]) = ???
+  def sort(a: List[Int], b: List[String]) = Action { request =>
+    Ok(s"result of sort $a and $b is ${(a.map(_.toString) ++ b).sorted.mkString(" ")}\n")
+  }
+  def sortN(a: List[Int]) = Action { request =>
+    Ok(s"result of sort $a is ${a.sorted.mkString(" ")}\n")
+  }
+  def sortS(a: List[String]) = Action { request =>
+    Ok(s"result of sort $a is ${a.sorted.mkString(" ")}\n")
+  }
+
 
   // TODO: Create an action called `howToAdd`:
   //
@@ -42,7 +58,10 @@ object CalcController extends Controller {
   //    HTTP method and URL required to add them together.
   //
   // TIP: Use the reverse route for `add()` to construct the URL.
-  def howToAdd(a: Int, b: Int) = ???
+  def howToAdd(a: Int, b: Int) = Action { request =>
+    val call = routes.CalcController.add(a, b)
+    Ok(s"result howToAdd $a $b '${call.url}'\n")
+  }
 
   private def urlDecode(str: String) =
     java.net.URLDecoder.decode(str, "UTF-8")
