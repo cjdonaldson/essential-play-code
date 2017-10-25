2d2815d3efa71abcd6bd2db0c46a9ce291d1eada
cdonaldson <charles.donaldson@arris.com>
2017-10-24 22:19:12 -0400

    chapter 3 form

--- chapter3-todo-form/app/controllers/TodoController.scala
+++ chapter3-todo-form/app/controllers/TodoController.scala
@@ -6,16 +6,37 @@ import play.api.data.Form
 import play.twirl.api.Html
 import models._
 
+import play.api.data.Form._
+import play.api.data.Forms._
+import play.api.data._
+import play.api.data.validation.Constraint
+// import play.api.data.validation.Constraints
+import play.api.data.validation.Constraints._ //min
+//import play.api.data.validation.Constraints._ //min
+
+import scala.util.matching.Regex
+
 object TodoController extends Controller with TodoDataHelpers {
   // TODO: Create a Form[Todo]:
   //  - build the basic form mapping;
   //  - create constraint to ensure the label is non-empty.
+  val uuidRegex: Regex =
+    "(?i:[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})".r
+
+  val uuidConstraint: Constraint[String] =
+    pattern(regex = uuidRegex, name = "UUID", error = "error.uuid")
+
+  val todoForm: Form[Todo] = Form(mapping(
+    "id"       -> optional(text.verifying(uuidConstraint)),
+    "label"    -> nonEmptyText,
+    "complete" -> boolean
+  )(Todo.apply)(Todo.unapply))
 
   def index = Action { request =>
-    Ok(renderTodoList(todoList))
+    Ok(renderTodoList(todoList, todoForm))
   }
 
-  def submitTodoForm = Action { request =>
+  def submitTodoForm = Action { implicit request =>
     // TODO: Write code to handle the form submission:
     //  - validate the form;
     //  - if form is valid:
@@ -23,12 +44,20 @@ object TodoController extends Controller with TodoDataHelpers {
     //     - redirect to index;
     //  - else:
     //     - display errors.
-    ???
+    todoForm.bindFromRequest().fold(
+      hasErrors = { errorForm =>
+        BadRequest(renderTodoList(todoList, errorForm))
+      },
+      success = { todo =>
+        todoList = todoList.addOrUpdate(todo)
+        Redirect(routes.TodoController.index)
+      }
+    )
   }
 
-  def renderTodoList(todoList: TodoList): Html =
+  def renderTodoList(todoList: TodoList, form: Form[Todo]): Html =
     // TODO: Modify template to show form:
-    views.html.todoList(todoList)
+    views.html.todoList(todoList, form)
 }
 
 trait TodoDataHelpers {
