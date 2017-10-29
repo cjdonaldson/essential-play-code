c487ef536a4c988bf10d6c6c08805734d959cb23
cdonaldson <charles.donaldson@arris.com>
2017-10-28 21:25:23 -0400

    chapter 4 animals

--- chapter4-animals/src/main/scala/JsonAnimalsExercise.scala
+++ chapter4-animals/src/main/scala/JsonAnimalsExercise.scala
@@ -1,5 +1,7 @@
 import play.api.libs.json._
-import play.api.libs.functional.syntax._
+// import play.api.libs.functional.syntax._
+
+import play.api.data.validation.ValidationError
 
 object JsonAnimalsExercise {
   sealed trait Animal
@@ -12,4 +14,22 @@ object JsonAnimalsExercise {
   //     - Creating JSON formats for Dog, Insect, and Swallow
   //     - Creating a JSON format for Animal that adds/uses a
   //       "type" field to distinguish between the three cases
+  val dogFormat = Json.format[Dog]
+  val insectFormat = Json.format[Insect]
+  val swallowFormat = Json.format[Swallow]
+
+  implicit object animalFormat extends Format[Animal] {
+    def writes(animal: Animal): JsValue = animal match {
+      case a: Dog => dogFormat.writes(a) ++ Json.obj("type" -> "Dog")
+      case a: Insect => insectFormat.writes(a) ++ Json.obj("type" -> "Insect")
+      case a: Swallow => swallowFormat.writes(a) ++ Json.obj("type" -> "Swallow")
+    }
+    def reads(j: JsValue): JsResult[Animal] = (j \ "type") match {
+      case JsString("Dog") => dogFormat.reads(j)
+      case JsString("Insect") => insectFormat.reads(j)
+      case JsString("Swallow") => swallowFormat.reads(j)
+      case other => JsError(JsPath \ "type", ValidationError("error.expected.animal.type"))
+    }
+
+  }
 }
