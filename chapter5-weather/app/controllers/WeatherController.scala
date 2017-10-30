610f9f3074c768930c70be862668d074459d9ab0
cdonaldson <charles.donaldson@arris.com>
2017-10-30 11:49:12 -0400

    chapter 5 weather

--- chapter5-weather/app/controllers/WeatherController.scala
+++ chapter5-weather/app/controllers/WeatherController.scala
@@ -23,7 +23,12 @@ object WeatherController extends Controller {
   // combine the results here and feed them to the template in `report.scala.html`:
   def report(location: String) =
     Action.async { request =>
-      ???
+      val weatherFuture = fetchWeather(location)
+      val forecastFuture = fetchForecast(location)
+      for {
+        weather <- weatherFuture
+        forecast <- forecastFuture
+      } yield Ok(views.html.report(location, weather, forecast))
     }
 
   // TODO: Complete this method. Use the WS API to gather data from the following endpoint,
@@ -31,12 +36,20 @@ object WeatherController extends Controller {
   //
   // GET http://api.openweathermap.org/data/2.5/weather?q=<location>,uk
   def fetchWeather(location: String): Future[Weather] =
-    ???
+    fetch[Weather]("weather", location)
 
   // TODO: Complete this method. Use the WS API to gather data from the following endpoint,
   // and parse it as an instance of `models.Forecast`:
   //
   // GET http://api.openweathermap.org/data/2.5/forecast?q=<location>,uk
   def fetchForecast(location: String): Future[Forecast] =
-    ???
+    fetch[Forecast]("forecast", location)
+
+  private def fetch[A: Reads](endpoint: String, location: String): Future[A] =
+    WS.url(s"http://api.openweathermap.org/data/2.5/$endpoint?q=$location,uk")
+      .withFollowRedirects(true)
+      .withRequestTimeout(5000)
+      .get()
+      .map (request => request.json.as[A])
+
 }
